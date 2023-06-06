7fa8607dfc620012125e47412c9b743efa4e0b25
AiSiN <nikitosina71ru@gmail.com>
2023-06-06 22:54:39 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Никита Александров on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
